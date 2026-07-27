505ac196fcb589553b81b850a2ddd09fa27b88ca
ishasuroshi2006-gif <ishasuroshi2006@gmail.com>
2026-07-27 10:05:42 +0530

    Create abstraction.py

---
+++ abstraction.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        print("Area:", self.side * self.side)
+
+s = Square(5)
+s.area()
